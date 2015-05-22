2666bea3c99398c9b50f641330542c5ced148ad0
Kelly Kiernan <kelly@kellykiernan.com>
2015-05-22 18:23:23 -0400

    Removed unused boot method

--- src/Sword/SwordServiceProvider.php
+++ src/Sword/SwordServiceProvider.php
@@ -5,16 +5,6 @@ use Illuminate\Support\ServiceProvider;
 class SwordServiceProvider extends ServiceProvider {
 
 	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		//
-	}
-
-	/**
 	 * Register the application services.
 	 *
 	 * @return void
